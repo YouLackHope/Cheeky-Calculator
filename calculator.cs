23a8809db0dc9fca65a1a7d6bd7d7b5249a0eae2
YouLackHope <114169301+YouLackHope@users.noreply.github.com>
2024-06-25 12:03:28 +0100

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -6,47 +6,53 @@ namespace DannyPractice
     {
         static void Main(string[] args)
         {
-            double num1 = 0;
-            double num2 = 0;
-            double result = 0;
-
-            Console.WriteLine("-----------");
-            Console.WriteLine("Calculator");
-            Console.WriteLine("-----------");
-
-            Console.Write("Enter number 1: ");
-            num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter number 2: ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter an Option: +, -, *, /");
-
-
-
-            switch (Console.ReadLine())
+            do
             {
-                case "+":
-                    result = num1 + num2;
-                    Console.Write($"The result of: {num1} + {num2}  = " + result);
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    Console.Write($"The result of: {num1} - {num2}  = " + result);
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    Console.Write($"The result of: {num1} * {num2}  = " + result);
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    Console.Write($"The result of: {num1} / {num2}  = " + result);
-                    break;
-                default:
-                    Console.WriteLine("That wasnt a valid option");
-                    break;
-            }
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
 
+                Console.WriteLine("-----------");
+                Console.WriteLine("Calculator");
+                Console.WriteLine("-----------");
+
+                Console.Write("Enter number 1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter an Option: +, -, *, /");
+
+
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"The result of: {num1} + {num2}  = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"The result of: {num1} - {num2}  = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"The result of: {num1} * {num2}  = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"The result of: {num1} / {num2}  = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("That wasnt a valid option");
+                        break;
+                }
+
+             Console.WriteLine("Clear (Y/N): ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.Write("Calculator closing!");
             Console.ReadKey();
         }
     }
